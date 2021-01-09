f45e71639975794f88abbb6409fccff3d251240b
seyyedAlirezaGhazanfari <59168138+seyyedAlirezaGhazanfari@users.noreply.github.com>
2021-01-09 16:26:56 +0330

    Advertiser is ready

--- src/Advertiser.java
+++ src/Advertiser.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
+
 public class Advertiser extends BaseAdvertising {
     private int id;
     private String name;
     private int clicks;
     private int views;
+    private static ArrayList<Advertiser> advertisers = new ArrayList<>();
 
     public String getName() {
         return name;
@@ -12,29 +15,23 @@ public class Advertiser extends BaseAdvertising {
         this.name = name;
     }
 
-    public int getClicks() {
-        return clicks;
-    }
-
-    public int getViews() {
-        return views;
-    }
-    public void incClicks(){
-        clicks++;
-    }
-    public void incViews(){
-        views++;
-    }
     public String help(){
         String helpInfo = "id is unique number of advertiser ***\nname is advertiser's name ***\nclciks is number of its clicks ***\nviews is number of its views ***\n";
         return helpInfo;
     }
-    public String describeMe(){
+
+    @Override
+    public String describeMe() {
         String classTask = "this class is implemented for advertisers and companies who want to advertise";
         return classTask;
     }
-    public static int getTotalClicks(){
 
+    public static int getTotalClicks(){
+        int totalClicks = 0;
+        for (Advertiser advertiser : advertisers) {
+            totalClicks+=advertiser.getClicks();
+        }
+        return totalClicks;
     }
 
 
