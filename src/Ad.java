2cf8d2efa42020f14462a3c9daf114b8fa505830
seyyedAlirezaGhazanfari <59168138+seyyedAlirezaGhazanfari@users.noreply.github.com>
2021-01-09 19:34:28 +0330

    Ad is ready

--- src/Ad.java
+++ src/Ad.java
@@ -1,2 +1,47 @@
-public class Ad {
+import java.net.URL;
+
+public class Ad extends BaseAdvertising {
+
+    private String title;
+    private String imgUrl;
+    private String link;
+    private Advertiser advertiser;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public Advertiser getAdvertiser() {
+        return advertiser;
+    }
+
+    public void setAdvertiser(Advertiser advertiser) {
+        this.advertiser = advertiser;
+    }
+
+    @Override
+    public String describeMe() {
+        return super.describeMe();
+    }
+
 }
